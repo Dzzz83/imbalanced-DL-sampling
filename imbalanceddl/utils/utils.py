2f9a5e36c7fb3a1b7760bab2f1c1e89be85e36a8
Dzzz83 <phattuan43@gmail.com>
2026-03-31 15:54:04 +0000

    fix bugs

--- imbalanceddl/utils/utils.py
+++ imbalanceddl/utils/utils.py
@@ -47,7 +47,7 @@ def prepare_folders(args):
     for folder in folders_util:
         if not os.path.exists(folder):
             print('creating folder ' + folder)
-            os.mkdirs(folder, exist_ok=True)
+            os.makedirs(folder, exist_ok=True)
 
 
 def prepare_store_name(args):
