bf64b70a0c99f20e83baf25a1a7c8d0053081c8d
Dzzz83 <phattuan43@gmail.com>
2026-04-01 04:14:41 +0000

    fix logging

--- imbalanceddl/utils/config.py
+++ imbalanceddl/utils/config.py
@@ -67,7 +67,7 @@ def get_args():
     # Data Selection Method (Random, LAVA, None)
     parser.add_argument('--selection_method', default=None, type=str, choices=['lava', 'random', None], 
                         help='Method for data selection/filtering')
-    parser.add_argument('--selection_ratio', default=0.7, type=float, 
+    parser.add_argument('--selection_ratio', default=None, type=float, 
                         help='Ratio of data to keep after selection')
     
     # update config from command line
